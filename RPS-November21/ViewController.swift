ad7f95e6a829e9cc65067ba71f110419499dcd63
Gianluca Orpello <g.orpello@gmail.com>
2021-11-29 12:08:41 +0100

    Complete the UI of the project.

--- RPS-November21/ViewController.swift
+++ RPS-November21/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var signLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var rockButton: UIButton!
+    @IBOutlet weak var paperButton: UIButton!
+    @IBOutlet weak var scissorsButton: UIButton!
+    
+    @IBOutlet weak var playAgainButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rockChosen(_ sender: UIButton) {
+    }
+    
+    @IBAction func paperChosen(_ sender: UIButton) {
+    }
+    
+    @IBAction func scissorChosen(_ sender: UIButton) {
+    }
+    
+    @IBAction func playAgain(_ sender: UIButton) {
+    }
+    
 }
 
